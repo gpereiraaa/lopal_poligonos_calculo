b9a0c421d8f5c5e866fb49e7e01995a5eb39f22a
GUSTAVO PEREIRA LEITE DA SILVA <25132694@CFP127ALUNO.LOCAL>
2025-03-12 16:46:29 -0300

    Classe Quadrado concluída.

---
+++ src/br/com/engsenai/projeto_formas/Main.java
@@ -0,0 +1,19 @@
+//Desenvolvido por Gustavo Pereira
+
+package br.com.engsenai.projeto_formas;
+
+import br.com.engsenai.projeto_formas.model.Quadrado;
+
+public class Main {
+
+	public static void main(String[] args) {		
+		
+		Quadrado q1= new Quadrado();
+		q1.setLado(5);
+		
+		q1.mostrarDados();
+		
+	}
+
+
+}
---
+++ src/br/com/engsenai/projeto_formas/model/Quadrado.java
@@ -0,0 +1,35 @@
+package br.com.engsenai.projeto_formas.model;
+
+public class Quadrado {
+	
+	public double lado;
+	
+	public void setLado(double lado) {
+		if (lado < 0) {
+			this.lado = 1;
+		} else {
+			this.lado = lado;
+		}
+	}
+	
+	private double calcularArea() {
+		double area = lado * lado;
+		return area;
+	}
+	
+	private double calcularPerimetro() {
+		double perimetro = lado * 4;
+		return perimetro;
+	}
+	
+	public void mostrarDados() {
+		System.out.println("---------------------");
+		System.out.println("Dados do quadrado:");
+		System.out.println("---------------------");
+		System.out.println("Lado:      " + lado);
+		System.out.println("Área:      " + calcularArea());
+		System.out.println("Perímetro: " + calcularPerimetro());
+		System.out.println("----------------------");
+	}
+	
+}
